f4c6fbec8fcd68509618a2ba79d870d37b427ebc
Dima Yanovskji <criska7@gmail.com>
2023-04-26 11:01:17 +0300

    Master
    add change background color navbar

--- src/App.tsx
+++ src/App.tsx
@@ -6,11 +6,26 @@ import { SelectedPage } from "@/shared/types";
 
 function App() {
   const [selectedPage, setSelectedPage] = useState<SelectedPage>(SelectedPage.Home);
+  const [isTopOfPage, setIsTopOfPage] = useState<boolean>(true);
 
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY === 0) {
+        setIsTopOfPage(true);
+        setSelectedPage(SelectedPage.Home)
+      }
+      if (window.scrollY !== 0) setIsTopOfPage(false)
+    }
+    window.addEventListener("scroll", handleScroll)
+    return () => window.removeEventListener("scroll", handleScroll)
+  }, []);
 
   return (
     <div className="app bg-gray-50">
-      <Navbar selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
+      <Navbar 
+        isTopOfPage={isTopOfPage}
+        selectedPage={selectedPage} 
+        setSelectedPage={setSelectedPage} />
     </div>
   );
 }
