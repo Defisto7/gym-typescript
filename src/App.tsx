3b71124ef29bab5c7d2010fb8982450480bf82bb
Dima Yanovskji <criska7@gmail.com>
2023-05-02 13:07:07 +0300

    Master
    add our classes

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,38 @@
 import Navbar from "@/scenes/navbar";
 import Home from "@/scenes/home";
 import Behefits from "@/scenes/behefits";
-import { useState, useEffect } from 'react';
+import OurClasses from "@/scenes/ourclasses";
+import { useState, useEffect } from "react";
 import { SelectedPage } from "@/shared/types";
 
-
-
 function App() {
-  const [selectedPage, setSelectedPage] = useState<SelectedPage>(SelectedPage.Home);
+  const [selectedPage, setSelectedPage] = useState<SelectedPage>(
+    SelectedPage.Home
+  );
   const [isTopOfPage, setIsTopOfPage] = useState<boolean>(true);
 
   useEffect(() => {
     const handleScroll = () => {
       if (window.scrollY === 0) {
         setIsTopOfPage(true);
-        setSelectedPage(SelectedPage.Home)
+        setSelectedPage(SelectedPage.Home);
       }
-      if (window.scrollY !== 0) setIsTopOfPage(false)
-    }
-    window.addEventListener("scroll", handleScroll)
-    return () => window.removeEventListener("scroll", handleScroll)
+      if (window.scrollY !== 0) setIsTopOfPage(false);
+    };
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
   }, []);
 
   return (
     <div className="app bg-gray-50">
-      <Navbar 
+      <Navbar
         isTopOfPage={isTopOfPage}
-        selectedPage={selectedPage} 
-        setSelectedPage={setSelectedPage} />
+        selectedPage={selectedPage}
+        setSelectedPage={setSelectedPage}
+      />
       <Home setSelectedPage={setSelectedPage} />
       <Behefits setSelectedPage={setSelectedPage} />
+      <OurClasses setSelectedPage={setSelectedPage} />
     </div>
   );
 }
--- src/shared/types.ts
+++ src/shared/types.ts
@@ -9,3 +9,8 @@ export enum SelectedPage {
     title: string;
     description: string;
   }
+  export interface ClassType {
+    name: string;
+    description?: string;
+    image: string;
+  }
